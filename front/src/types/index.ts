3dd5d824751376aeadbdd0bef71cab32b2b50476
Omar Gomez <zero_reploid@hotmail.com>
2025-05-22 12:48:03 -0600

    Home: se agregó el home. Mock: se agregó el mock_products y se hizo una correción en el tipo de archivo y de código para que funcionara correctamente.

---
+++ front/src/types/index.ts
@@ -0,0 +1,56 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    imgUrl: string;  
+    category: string;
+}
+
+export interface ICategory{
+    id: number;
+    name: string;
+}
+export interface IloginProps{
+    email: string;
+    password: string;
+}
+export interface IloginErrors{
+    email?: string;
+    password?: string;
+}
+
+export interface IRegisterProps{
+    email: string;
+    password: string;
+    name: string;
+    address: string;
+    phone: string;
+}
+export interface IRegisterErrors{
+    email?: string;
+    password?: string;
+    name?: string;
+    address?: string;
+    phone?: string;
+}
+
+export interface IUserSession {
+    token: string;
+    user: {
+        address: string;
+        email: string;
+        id: number;
+        name: string;
+        phone: string;
+        orders: IOrder[];
+    };
+}
+
+export interface IOrder {
+    id: number;
+    status: string;
+    date: Date;
+    products: IProduct[];
+}
