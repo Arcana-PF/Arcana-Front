1fb0b2f628fbeaf1db51eef4347b7b5a35ca3ce5
Omar Gomez <zero_reploid@hotmail.com>
2025-06-11 21:31:10 -0600

    Actulizando Deploy

--- front/src/utils/Orders.helper.ts
+++ front/src/utils/Orders.helper.ts
@@ -1,23 +1,32 @@
 const APIURL = process.env.NEXT_PUBLIC_API_URL;
 
+if (!APIURL) {
+  throw new Error('API URL is not defined');
+}
+
 export async function createOrder(products: number[], token: string) {
   try {
     const response = await fetch(`${APIURL}/orders`, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
-        Authorization: token
+        Authorization: token,
       },
-      body: JSON.stringify({
-        products
-      })
+      body: JSON.stringify({ products }),
     });
 
-    if (response.ok) {
-      alert('Buy successfully');
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
     }
-  } catch (error: any) {
-    throw new Error(error);
+
+    alert('Buy successfully');
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      throw new Error(error.message);
+    } else {
+      throw new Error('Unexpected error occurred');
+    }
   }
 }
 
@@ -26,12 +35,22 @@ export async function getOrders(token: string) {
     const response = await fetch(`${APIURL}/orders`, {
       method: 'GET',
       headers: {
-        "Content-type": "application/json",
-        Authorization: token
+        'Content-type': 'application/json',
+        Authorization: token,
       },
     });
-    return response.json();
-  } catch (error: any) {
-    throw new Error(error);
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      throw new Error(error.message);
+    } else {
+      throw new Error('Unexpected error occurred');
+    }
   }
 }
